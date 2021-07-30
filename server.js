1c6dcc7d7904903fd8a560a8c1cc8edc8086d190
noa <noa.olivette@epitech.eu>
2021-07-30 12:50:12 +0400

    test

---
+++ server.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get("/", function (req, res){
+  res.sendFile(__dirname + '/index.html');
+})
+
+io.on('connection', function (socket){
+    console.log(" a user connected");
+    socket.on('disconnect', function (){
+        console.log('a user is disconnect')
+    })
+    socket.on('chat message', function (msg){
+        console.log(('message recu : ' + msg))
+        io.emit('chat message', msg);
+    })
+})
+
+http.listen(3068, function() {
+    console.log("Server running on, 3068")
+})
